683962c215ebed26000dcf4e5e2124d74e13feee
Robert <r_lee94@yahoo.com>
2019-04-11 03:27:09 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,4 +1,4 @@
-class Artist
+class Author
   attr_accessor :name
 
   @@all = []
@@ -12,20 +12,20 @@ class Artist
     @@all
   end
 
-  def add_song(song)
-    song.artist = self
+  def add_post(post)
+    post.author = self
   end
 
-  def add_song_by_name(name)
-    song = Song.new(name)
-    song.artist = self
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
   end
 
-  def songs
-    Song.all.select {|song| song.artist == self}
+  def posts
+    Post.all.select {|post| post.author == self}
   end
 
-  def self.song_count
-    Song.all.count
+  def self.post_count
+    Post.all.count
   end
 end
