03adc8296fbb6d78c97b736bce75774292529636
Robert <r_lee94@yahoo.com>
2019-04-11 02:49:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,9 +18,14 @@ class Artist
   
   def add_song_by_name(name)
     song = Song.new(name)
-    @songs >> song
     song.artist = self
-end
+  end
+  
+#   def add_song_by_name(name)
+#     song = Song.new(name)
+#     @songs >> song
+#     song.artist = self
+# end
 
    
 end
